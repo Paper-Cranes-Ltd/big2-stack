6d0bf7b420004780cf7014808f24db71a6883ad1
Hristo Stamenov <capitane71@gmail.com>
2023-03-27 18:31:41 +0300

    chore: Fix utility cmake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -24,6 +24,12 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
 
     file(MAKE_DIRECTORY "${SHADERS_OUT_DIR}")
 
+    # 120|300_es|spirv|metal|s_3_0|s_4_0|s_5_0
+    set(PROFILES 120 300_es spirv s_3_0 s_4_0 s_5_0)
+    if(APPLE)
+        list(APPEND PROFILES metal)
+    endif()
+
     file(GLOB VERTEX_SHADER_FILES CONFIGURE_DEPENDS FOLLOW_SYMLINKS "${SHADERS_DIR}/vs_*[!.def].sc")
     bgfx_compile_shader_to_header(
             TYPE VERTEX
@@ -31,6 +37,7 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
             VARYING_DEF "${VARYING_DEF_LOCATION}"
             OUTPUT_DIR "${SHADERS_OUT_DIR}"
             OUT_FILES_VAR VERTEX_OUTPUT_FILES
+            PROFILES ${PROFILES}
             INCLUDE_DIRS "${SHADERS_DIR}" "${BGFX_DIR}/src"
     )
 
@@ -41,10 +48,16 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
             VARYING_DEF "${VARYING_DEF_LOCATION}"
             OUTPUT_DIR "${SHADERS_OUT_DIR}"
             OUT_FILES_VAR FRAGMENT_OUTPUT_FILES
+            PROFILES ${PROFILES}
             INCLUDE_DIRS "${SHADERS_DIR}" "${BGFX_DIR}/src"
     )
     
-    set(OUTPUT_FILES ${VERTEX_OUTPUT_FILES} ${FRAGMENT_OUTPUT_FILES})
+    set(OUTPUT_FILES)
+    list(APPEND OUTPUT_FILES ${VERTEX_OUTPUT_FILES})
+    list(APPEND OUTPUT_FILES ${FRAGMENT_OUTPUT_FILES})
+
+    message(STATUS ${OUTPUT_FILES})
+
     list(LENGTH OUTPUT_FILES SHADER_COUNT)
     if(SHADER_COUNT EQUAL 0)
         return()
@@ -61,8 +74,10 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
     
     string(MD5 DIR_HASH "${SHADERS_DIR}")
     set(TARGET_NAME "Shaders_${DIR_HASH}")
+    add_custom_target("${DIR_HASH}" ALL SOURCES ${OUTPUT_FILES})
+
     add_library("${TARGET_NAME}" INTERFACE ${OUTPUT_FILES})
-    add_dependencies("${TARGET_NAME}" shaderc)
+    add_dependencies("${TARGET_NAME}" shaderc "${DIR_HASH}")
     target_include_directories("${TARGET_NAME}" INTERFACE "${CMAKE_CURRENT_BINARY_DIR}/include")
 
     set("${TARGET_OUT_VAR}" "${TARGET_NAME}" PARENT_SCOPE)

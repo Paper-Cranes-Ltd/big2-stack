f9a3a02d0f73f3cb83a69ed52c1eec5aa7eab945
Hristo Stamenov <capitane71@gmail.com>
2023-04-04 14:05:01 +0300

    chore: Improvement on shader compilation utility

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -8,18 +8,26 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
         message(NOTICE "Shaders directory does not exist")
         return()
     endif()
-    
+
+    file(GLOB VERTEX_SHADER_FILES CONFIGURE_DEPENDS FOLLOW_SYMLINKS "${SHADERS_DIR}/vs_*[!.def].sc")
+    file(GLOB FRAGMENT_SHADER_FILES CONFIGURE_DEPENDS FOLLOW_SYMLINKS "${SHADERS_DIR}/fs_*[!.def].sc")
+
+    if(NOT VERTEX_SHADER_FILES AND NOT FRAGMENT_SHADER_FILES)
+        message(NOTICE "No shader files in directory")
+        return()
+    endif()
+
     if(CMAKE_CROSSCOMPILING AND NOT("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "${CMAKE_SYSTEM_NAME}"))
         message(STATUS "Not compiling shaders during cross-compilation")
         return()
     endif()
-    
+
     set(VARYING_DEF_LOCATION "${SHADERS_DIR}/varying.def.sc")
     if(NOT EXISTS "${VARYING_DEF_LOCATION}")
         message(WARNING "Varying def does not exist")
         return()
     endif()
-    
+
     set(SHADERS_OUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/include/generated/shaders/${NAMESPACE}")
 
     file(MAKE_DIRECTORY "${SHADERS_OUT_DIR}")
@@ -30,7 +38,6 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
         list(APPEND PROFILES metal)
     endif()
 
-    file(GLOB VERTEX_SHADER_FILES CONFIGURE_DEPENDS FOLLOW_SYMLINKS "${SHADERS_DIR}/vs_*[!.def].sc")
     bgfx_compile_shader_to_header(
             TYPE VERTEX
             SHADERS ${VERTEX_SHADER_FILES}
@@ -41,7 +48,6 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
             INCLUDE_DIRS "${SHADERS_DIR}" "${BGFX_DIR}/src"
     )
 
-    file(GLOB FRAGMENT_SHADER_FILES CONFIGURE_DEPENDS FOLLOW_SYMLINKS "${SHADERS_DIR}/fs_*[!.def].sc")
     bgfx_compile_shader_to_header(
             TYPE FRAGMENT
             SHADERS ${FRAGMENT_SHADER_FILES}
@@ -51,7 +57,7 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
             PROFILES ${PROFILES}
             INCLUDE_DIRS "${SHADERS_DIR}" "${BGFX_DIR}/src"
     )
-    
+
     set(OUTPUT_FILES)
     list(APPEND OUTPUT_FILES ${VERTEX_OUTPUT_FILES})
     list(APPEND OUTPUT_FILES ${FRAGMENT_OUTPUT_FILES})
@@ -60,7 +66,7 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
     if(SHADER_COUNT EQUAL 0)
         return()
     endif()
-    
+
     set(INCLUDE_ALL_HEADER "")
     foreach(OUTPUT_FILE IN LISTS OUTPUT_FILES)
         get_filename_component(OUTPUT_FILENAME "${OUTPUT_FILE}" NAME)
@@ -68,10 +74,12 @@ function(add_shaders_directory SHADERS_DIR TARGET_OUT_VAR)
     endforeach()
     file(WRITE "${SHADERS_OUT_DIR}/all.h" "${INCLUDE_ALL_HEADER}")
     list(APPEND OUTPUT_FILES "${SHADERS_OUT_DIR}/all.h")
-    
+
     string(MD5 DIR_HASH "${SHADERS_DIR}")
     set(TARGET_NAME "Shaders_${DIR_HASH}")
-    add_custom_target("${DIR_HASH}" ALL DEPENDS ${OUTPUT_FILES})
+    add_custom_target("${DIR_HASH}" ALL
+            DEPENDS ${OUTPUT_FILES}
+            SOURCES ${VARYING_DEF_LOCATION} ${FRAGMENT_SHADER_FILES} ${VERTEX_SHADER_FILES})
 
     add_library("${TARGET_NAME}" INTERFACE)
     add_dependencies("${TARGET_NAME}" shaderc "${DIR_HASH}")

10f3a071ae0c73103635538004f35ba1d73a4cc7
Hristo Stamenov <capitane71@gmail.com>
2023-05-08 11:12:01 +0300

    chore: Removed patch and converted to a better local approach.

--- cmake/bgfx.cmake
+++ cmake/bgfx.cmake
@@ -2,7 +2,6 @@ fetchcontent_declare(
         bgfx
         GIT_REPOSITORY "https://github.com/bkaradzic/bgfx.cmake.git"
         GIT_TAG "v1.118.8455-424"
-        PATCH_COMMAND git reset --hard && git apply --ignore-space-change --ignore-whitespace --allow-empty "${CMAKE_CURRENT_SOURCE_DIR}/patches/bgfx-cmake.patch"
         ${BIG2_COMMON_DEPS_OPTIONS}
 )
 

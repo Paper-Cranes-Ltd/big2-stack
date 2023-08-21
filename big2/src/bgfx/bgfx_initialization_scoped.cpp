23db676ea2f6b7ecf86e80e20252f5da8bbd189c
Anonymous <>
2023-08-22 00:44:16 +0300

    chore: Enable vsync and depth clamp reset by default.

--- big2/src/bgfx/bgfx_initialization_scoped.cpp
+++ big2/src/bgfx/bgfx_initialization_scoped.cpp
@@ -13,6 +13,7 @@ namespace big2 {
 BgfxInitializationScoped::BgfxInitializationScoped(bgfx::RendererType::Enum renderer_type) {
   bgfx::Init init_object;
   init_object.type = renderer_type;
+  init_object.resolution.reset = BGFX_RESET_VSYNC | BGFX_RESET_DEPTH_CLAMP;
   big2::SetNativeData(init_object);
   big2::Validate(bgfx::init(init_object), "BGFX couldn't be initialized");
 }

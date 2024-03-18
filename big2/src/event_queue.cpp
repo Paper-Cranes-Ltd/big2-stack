4ddcc65807bbef04117e7c476e11f31587e247c6
Anonymous <>
2024-03-18 21:41:24 +0200

    fix: Fixes an issue when compiling on Linux using GCC
    Should resolve #4 (to be checked)

--- big2/src/event_queue.cpp
+++ big2/src/event_queue.cpp
@@ -178,7 +178,7 @@ gsl::span<GlfwEvent> GrabEvents(gsl::not_null<GLFWwindow *> window) {
 
 static void SortEventsByWindow() {
   using TRef = typename decltype(events)::reference;
-  const std::invocable<TRef, TRef> auto &&sorter = [](TRef left, TRef right) noexcept { return left.window > right.window; };
+  const std::invocable<const TRef, const TRef> auto &&sorter = [](const TRef left, const TRef right) noexcept { return left.window > right.window; };
   std::sort(EXECUTION_POLICY(std::execution::par_unseq) events.begin(), events.end(), sorter);
 }
 
